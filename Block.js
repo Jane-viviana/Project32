8f8107847402c4b592d63f68137316d1be432b57
Jane <73689728+Jane-viviana@users.noreply.github.com>
2021-01-22 15:49:07 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,43 @@
+class Block{
+    constructor(x,y,width,height){
+        var options = {
+            //isStatic:true,
+            'restitution':0.8,
+            'friction':2.0,
+            'density':2.0
+        }
+        this.visibility = 225;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+        
+    }
+    display(){
+        if(this.body.speed<5){
+            var angle = this.body.angle;
+            var pos = this.body.position;
+            push();
+           translate(this.body.position.x,this.body.position.y);
+            rotate(angle);
+            rectMode(CENTER);
+            stroke("maroon");
+            fill("white")
+            rect(0,0, this.width, this.height);
+            pop();
+        }
+
+        else{
+            World.remove(world,this.body);
+            push();
+            this.visibility = this.visibility - 5
+            tint(255,this.visibility);
+            pop();
+        }
+    }
+        score(){
+            if(this.visibility < 0 && this.visiblity >- 1005){
+              score++
+            }
+        }
+    }
---
+++ sketch.js
@@ -0,0 +1,107 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground;
+var ground2;
+var block1,block2,block3,block4;
+var block5,block6,block7,block8;
+var block9,block10,block11;
+var polygon;
+var sling;
+var score = 0;
+var bkg;
+var bg;
+
+function preload(){
+    getBackGroundImg()
+    bkg = loadImage("Night sky.jpg");
+    bg = loadImage("sky day.jpg");
+}
+
+
+function setup(){
+     var canvas = createCanvas(800,500);
+     engine = Engine.create()
+     world = engine.world;
+
+     ground = new Ground(475,400,300,15);
+     block1 = new Block(475,300,50,50);
+     block2 = new Block(450,300,50,50)
+     block3 = new Block(450,300,50,50);
+     block4 = new Block(475,300,50,50);
+     
+     ground2 = new Ground(600,200,300,15);
+     block5 = new Block(600,100,50,50);
+     block6 = new Block(600,100,50,50);
+     block7 = new Block(600,100,50,50);
+     block8 = new Block(650,20,50,50);
+     block9 = new Block(650,20,50,50);
+     block10 = new Block(550,20,50,50);
+     block11 = new Block(550,20,50,50);
+
+     polygon = new Polygon(175,250,15,15);
+     sling = new SlingShot(polygon.body,{x:175,y:250});
+}
+function draw(){
+    if(bkg){
+        background(bg);
+   }
+   else{
+       background(0);
+   }
+    noStroke();
+    textSize(25)
+    fill("white")
+    text("Score  " + score, width-700, 50)
+
+    Engine.update(engine);
+
+    ground.display();
+    block1.display();
+    block2.display();
+    block3.display();
+    block4.display();
+    ground2.display();
+    block5.display();
+    block6.display();
+    block7.display();
+    block8.display();
+    block9.display();
+    block10.display();
+   block11.display();
+
+    polygon.display();
+    sling.display();
+}
+function mouseDragged(){
+    Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    sling.fly();
+}
+
+function keyPressed(){
+    
+    if(keyCode === 32){
+
+    sling.attach(polygon.body);
+    }
+}
+
+async function getBackGroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+
+    if(hour>0600 && hour<1600){
+      bkg 
+    }
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
